b8753bcebd087bd96ed5b9f5bd2b928fc0e31bc5
Maciej Mikosik <maciej@mikosik.com>
2017-05-04 11:35:13 +0200

    created interface representing computation

---
+++ main/java/com/perunlabs/mokosh/run/Running.java
@@ -0,0 +1,35 @@
+package com.perunlabs.mokosh.run;
+
+import java.util.function.Supplier;
+
+import com.perunlabs.mokosh.AbortException;
+
+/**
+ * Represents parallel execution.
+ *
+ * @param <T>
+ *          type of result of execution
+ */
+public interface Running<T> {
+  /**
+   * Blocks until execution is completed or aborted. Supplied result of execution might return
+   * result or throw exception.
+   *
+   * @throws AbortException
+   *           if waiting thread was interrupted
+   */
+  Supplier<T> await();
+
+  /**
+   * Tries to abort underlying execution.
+   *
+   * May not be successful if execution is already completed or it does not respond to interruption.
+   * If execution was already aborted then nothing happens.
+   */
+  Running<T> abort();
+
+  /**
+   * @return true if execution is still running, false if it is completed or aborted
+   */
+  boolean isRunning();
+}
