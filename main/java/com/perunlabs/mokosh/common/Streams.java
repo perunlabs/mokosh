b9cafa8b3eb73f2125b526f8391d6bb186d4a43f
Maciej Mikosik <maciej@mikosik.com>
2017-05-19 01:04:34 +0200

    disabled buffering when pumping

--- main/java/com/perunlabs/mokosh/common/Streams.java
+++ main/java/com/perunlabs/mokosh/common/Streams.java
@@ -1,23 +1,18 @@
 package com.perunlabs.mokosh.common;
 
 import static com.perunlabs.mokosh.AbortException.abortIfInterrupted;
-import static com.perunlabs.mokosh.common.Unchecked.unchecked;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class Streams {
-  public static void pump(InputStream input, OutputStream output) {
-    try {
-      int count;
-      byte[] buffer = new byte[8 * 1024];
-      while ((count = input.read(buffer)) != -1) {
-        abortIfInterrupted();
-        output.write(buffer, 0, count);
-      }
-    } catch (IOException e) {
-      throw unchecked(e);
+  public static void pump(InputStream input, OutputStream output) throws IOException {
+    int oneByte;
+    while ((oneByte = input.read()) != -1) {
+      output.write(oneByte);
+      abortIfInterrupted();
     }
+    output.flush();
   }
 }
