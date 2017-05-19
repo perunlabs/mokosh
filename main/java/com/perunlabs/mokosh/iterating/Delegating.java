fe6807cab7c2d1e1f7950b1a684095f4476db952
Maciej Mikosik <maciej@mikosik.com>
2017-05-19 08:16:34 +0200

    improved generic handling in delegating iterating

--- main/java/com/perunlabs/mokosh/iterating/Delegating.java
+++ main/java/com/perunlabs/mokosh/iterating/Delegating.java
@@ -9,26 +9,27 @@ import java.util.function.Supplier;
 import com.perunlabs.mokosh.running.Running;
 
 public class Delegating<E> implements Iterating<E> {
-  private final Running<Void> running;
+  private final Running<?> running;
   private final Iterator<E> iterator;
 
-  private Delegating(Running<Void> running, Iterator<E> iterator) {
+  private Delegating(Running<?> running, Iterator<E> iterator) {
     this.running = running;
     this.iterator = iterator;
   }
 
-  public static <E> Iterating<E> iterating(Running<Void> running, Iterator<E> iterator) {
+  public static <E> Iterating<E> iterating(Running<?> running, Iterator<E> iterator) {
     check(running != null);
     check(iterator != null);
     return new Delegating<>(running, iterator);
   }
 
   public Supplier<Void> await() {
-    return running.await();
+    return nullify(running.await());
   }
 
   public Running<Void> abort() {
-    return running.abort();
+    running.abort();
+    return this;
   }
 
   public boolean isRunning() {
@@ -45,5 +46,12 @@ public class Delegating<E> implements Iterating<E> {
 
   public String toString() {
     return format("iterating(%s, %s)", running, iterator);
+  }
+
+  private static Supplier<Void> nullify(Supplier<?> supplier) {
+    return () -> {
+      supplier.get();
+      return null;
+    };
   }
 }
